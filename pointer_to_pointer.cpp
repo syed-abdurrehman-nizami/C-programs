aeeae41f1e60f1307a0ed654f569a92c7b2ac3a0
Syed Abdurrehman Nizami <syedabdurrehman859@gmail.com>
2023-12-17 08:11:28 +0500

    initial commmit

--- pointer_to_pointer.cpp
+++ pointer_to_pointer.cpp
@@ -9,23 +9,24 @@ int main()
     int **c = &b;
     int ***d = &c;
 
-    cout<<a<<endl;     // print value of a
-    cout<<b<<endl;     // print address of a
-    cout<<*b<<endl;    // print value at stored address.
-    cout<<c<<endl;     // print address of b
-    cout<<*c<<endl;    // print address which is stored in b
-    cout<<**c<<endl;   // Reach the address stored in b then print the value present at that address
+    cout << a << endl;   // print value of a
+    cout << b << endl;   // print address of a
+    cout << *b << endl;  // print value at stored address.
+    cout << c << endl;   // print address of b
+    cout << *c << endl;  // print address which is stored in b
+    cout << **c << endl; // Reach the address stored in b then print the value present at that address
 
-    cout<<endl<<endl;
-    cout<<"Adderss of a : "<<b<<endl;
-    cout<<"Adderss of b : "<<c<<endl;
-    cout<<"Adderss of c : "<<d<<endl;
+    cout << endl;
+        
+    cout << "Adderss of a : " << b << endl;
+    cout << "Adderss of b : " << c << endl;
+    cout << "Adderss of c : " << d << endl;
 
-    cout<<endl;
-    cout<<d<<endl;      // print address of c
-    cout<<*d<<endl;     // print address of b
-    cout<<**d<<endl;    // print address of a
-    cout<<***d<<endl;   // print the value of a
+    cout << endl;
+    cout << d << endl;    // print address of c
+    cout << *d << endl;   // print address of b
+    cout << **d << endl;  // print address of a
+    cout << ***d << endl; // print the value of a
 
     return 0;
 }
